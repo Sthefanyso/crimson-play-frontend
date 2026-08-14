91a64bd7886c1d8cc9f31d7307d08b079c105ca8
Sthefanyso <sthefanydsilva29@gmail.com>
2026-08-13 22:29:23 -0300

    style: add style for navbar and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 function Footer() {
   return (
-    <footer>
+    <footer className="h-20 w-full bg-neutral-700 text-neutral-0">
+      <div className="mx-auto flex max-w-7xl items-center h-full justify-between px-6">
       <div>
         <span>Logo</span>
       </div>
@@ -9,13 +10,14 @@ function Footer() {
         This product uses the TMDB API but is not endorsed or certified by TMDB.
       </p>
 
-      <div>
+      <div className="flex gap-4">
         <a href="#">GitHub</a>
         <a href="#">Email</a>
         <a href="#">LinkedIn</a>
       </div>
 
       <span>© 2026 CrimsonPlay</span>
+      </div>
     </footer>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,16 +2,19 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
+    <nav className="h-20 w-full bg-neutral-700 text-neutral-0">
+      <div className="flex h-full items-center px-6">
       <span>Logo</span>
-      <div>
+
+      <div className="ml-12 flex gap-8">
         <NavLink to="/movies">Filmes</NavLink>
         <NavLink to="/series">Séries</NavLink>
       </div>
 
-      <div>
+      <div className="ml-auto flex items-center gap-8">
         <button>Busca</button>
         <button>Usuário</button>
+      </div>
       </div>
     </nav>
   );
